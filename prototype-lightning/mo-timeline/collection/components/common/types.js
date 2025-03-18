76da4e6265f2cf644859fd033053a3d7771733f5
Ian Watkins <ian.watkins@metoffice.gov.uk>
2025-03-18 15:22:09 +0000

    Public Website 1995

--- prototype-lightning/mo-timeline/collection/components/common/types.js
+++
@@ -1,6 +0,0 @@
-export var EWarningSeverity;
-(function (EWarningSeverity) {
-    EWarningSeverity["RED"] = "RED";
-    EWarningSeverity["AMBER"] = "AMBER";
-    EWarningSeverity["YELLOW"] = "YELLOW";
-})(EWarningSeverity || (EWarningSeverity = {}));
